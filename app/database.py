8da96e1fb2e33c8b90c5eb3ae56d0bccb0156abd
Drake Forum <devanshchaturvedi646@gmail.com>
2026-04-06 00:24:47 +0530

    chore: apply requested updates

--- app/database.py
+++ app/database.py
@@ -3,15 +3,10 @@ from collections.abc import Generator
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 load_dotenv()
 
-DATABASE_USER = os.getenv("DATABASE_USER", "postgres")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "postgres")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "5432")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "url_shortener")
 DATABASE_POOL_MAX = int(os.getenv("DATABASE_POOL_MAX", "10"))
 
 DATABASE_URL = os.environ.get(
@@ -28,10 +23,7 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-
-class Base(DeclarativeBase):
-    pass
-
+Base = declarative_base()
 
 def get_db() -> Generator[Session, None, None]:
     with SessionLocal() as session:
